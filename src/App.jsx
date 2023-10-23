32ae59df8cc51c1d06b2eaacf607ec559e614a0f
BranTkS <brantshumba@gmail.com>
2023-10-23 19:36:10 +0200

    config changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,16 @@
-import { useState } from 'react';
+//import { useState } from 'react';
 import './App.scss'
 //import FullResume from './components/FullResume'
 import RenderScene from './components/canvasAnimation/RenderScene'
 import Portfolio from './components/portfolio';
 import Navbar from './components/portfolio/navbar/navbar';
 
+//import { useEffect } from 'react';
+//import LoadingScreen from './components/LoadingScreen';
 
-import {
-  createBrowserRouter,
-  Route,
-  Outlet,
-  RouterProvider,
-  createRoutesFromElements
-} from 'react-router-dom'
-import { useEffect } from 'react';
-import LoadingScreen from './components/LoadingScreen';
-
-const returnPage = <main><Outlet /></main>;
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={returnPage}>
-      <Route index element=
-        {
-          <div>
-            <div className="outerNavbar">
-              <Navbar />
-            </div>
-
-            <Portfolio />
-            <RenderScene />
-          </div>
-        }
-      />
-      {/*<Route path="/resume" element={<FullResume />} />*/}
-    </Route>
-  )
-)
 
 function App() {
-  const [loading, setLoading] = useState(false);
+  { /* const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setLoading(true)
@@ -47,15 +18,16 @@ function App() {
       setLoading(false)
     }, 3000);
 
-  }, [])
+  }, [])*/}
 
   return (
     <div>
-      {loading ?
-        <LoadingScreen loading={loading} />
-        :
-        <RouterProvider router={router} />
-      }
+      <div className="outerNavbar">
+        <Navbar />
+      </div>
+
+      <Portfolio />
+      <RenderScene />
     </div>
   )
 }
