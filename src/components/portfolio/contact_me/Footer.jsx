280994fc45a6dfc35fd1b256b217ead742ca4e45
BranTkS <brantshumba@gmail.com>
2023-10-27 23:39:16 +0200

    adding links for icons

--- src/components/portfolio/contact_me/Footer.jsx
+++ src/components/portfolio/contact_me/Footer.jsx
@@ -14,11 +14,20 @@ const Footer = () => {
                     {githubSVG}
                     {deviantartSVG}
 
+
                     <div>some icons and svg&apos;s used are from  <a target="_blank" rel="noreferrer" href="https://icons8.com">Icons8</a> including <a target="_blank" rel="noreferrer" href="https://icons8.com/icon/59952/sql">SQL</a>,
 
                         <a target="_blank" rel="noreferrer" href="https://icons8.com/icon/11169/certificate">Certificate</a>,
 
                         <a target="_blank" rel="noreferrer" href="https://icons8.com/icon/44770/workstation">Computer</a>,
+
+                        <a target="_blank" rel="noreferrer" href="https://icons8.com/icon/44024/menu">Menu</a>,
+
+                        <a target="_blank" rel="noreferrer" href="https://icons8.com/icon/43969/about">About</a>,
+
+                        <a target="_blank" rel="noreferrer" href="https://icons8.com/icon/43988/code">Code</a>,
+
+                        <a target="_blank" rel="noreferrer" href="https://icons8.com/icon/9TWrjbfl_wCv/call-me">Call Me</a>
 
                     </div>
                     <div className="copyRight"> © {new Date().getFullYear()} - developed by Tanaka Brandon Shumba</div>
